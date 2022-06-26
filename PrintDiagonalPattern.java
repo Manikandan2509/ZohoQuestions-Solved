6ee36ae16342dc4fc167dedc925f331555f81ae5
Manikandan <82508909+Manikandan2509@users.noreply.github.com>
2022-06-26 13:32:50 +0530

    Add files via upload

---
+++ PrintDiagonalPattern.java
@@ -0,0 +1,23 @@
+package src;
+
+public class PrintDiagonalPattern {
+    public static void main(String[] args) {
+        char arr[]  = {'p','r','o','g','r','a','m'};
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                if(i==j) {
+                    System.out.print(arr[i]+" ");
+                }
+                else if(i+j == arr.length-1){
+                    System.out.print(arr[j]+" ");
+                }
+                else{
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+
+}
