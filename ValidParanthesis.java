db8a530ec988787f2c04a255caec95c475ade680
Manikandan <82508909+Manikandan2509@users.noreply.github.com>
2022-06-27 18:51:19 +0530

    Add files via upload

---
+++ ValidParanthesis.java
@@ -0,0 +1,29 @@
+package src;
+import java.util.*;
+public class ValidParanthesis {
+    public static void main(String[] args) {
+        String s = "{{";
+        System.out.println(isValid(s));
+    }
+    public static boolean isValid(String s){
+        Stack<Character> stack = new Stack();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
+                stack.push(s.charAt(i));
+            }
+            else if (s.charAt(i) == ')' && stack.peek() == '(') {
+                stack.pop();
+            }
+            else if (s.charAt(i) == ']' && stack.peek() == '[') {
+                stack.pop();
+            }
+            else if (s.charAt(i) == '}' && stack.peek() == '{') {
+                stack.pop();
+            }
+            else{
+                stack.push(s.charAt(i));
+            }
+        }
+        return stack.isEmpty();
+    }
+}
